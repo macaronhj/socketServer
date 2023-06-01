3f88c5e957b4ed5400cf9a43bd9dc082bb54fe63
macaronhj <huhheejin249@gmail.com>
2023-06-01 13:37:04 +0900

    프로젝트 생성

---
+++ src/main/java/com/heejin/socketServer/SocketServerApplication.java
@@ -0,0 +1,13 @@
+package com.heejin.socketServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heejin/socketServer/SocketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heejin.socketServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

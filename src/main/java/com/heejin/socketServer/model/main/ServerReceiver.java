e9c4bffbd5fb4f12235724912e1e1ea4f5ecab39
jklee <jklee@LAPTOP-618JVK7J>
2023-06-02 17:19:23 +0900

    로그인 성공

--- src/main/java/com/heejin/socketServer/model/main/ServerReceiver.java
+++ src/main/java/com/heejin/socketServer/model/main/ServerReceiver.java
@@ -1,13 +1,26 @@
 package com.heejin.socketServer.model.main;
 
-import com.heejin.socketServer.model.Protocol;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.io.*;
-import java.net.Socket;
-import java.net.URL;
-import java.util.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 
 public class ServerReceiver extends Thread{
@@ -44,9 +57,41 @@ public class ServerReceiver extends Thread{
             run_start://while문같은 반복문 전체를 빠져 나가도록 처리할 때
             while (!isStop) {
 
-                String msg = ois.readObject().toString(); //클라이언트로 부터 오는 메세지 수신 담당, 항상 메세지를 받은 이후부터 모든 서버업무가 수행이 가능함
-				logger.info("{} - 메세지를 수신했습니다.", msg);
-
+//                String msg = ois.readObject().toString(); //클라이언트로 부터 오는 메세지 수신 담당, 항상 메세지를 받은 이후부터 모든 서버업무가 수행이 가능함
+				Map<String, String> map = (Map<String, String>) ois.readObject();
+				String id = map.get("id");
+				String pwd = map.get("pwd");
+				
+				try {
+		            File file = new File("D:/00.heejin/socketServer/src/main/resources/user-info.xml");
+		            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		            DocumentBuilder db = dbf.newDocumentBuilder();
+		            Document document = db.parse(file);
+		            document.getDocumentElement().normalize();
+		            logger.info("Root Element :{}", document.getDocumentElement().getNodeName());
+		            NodeList nList = document.getElementsByTagName("user");
+		            logger.info("----------------------------");
+		            for (int temp = 0; temp < nList.getLength(); temp++) {
+		                Node nNode = nList.item(temp);
+		                logger.info("Current Element :{}", nNode.getNodeName());
+		                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+		                    Element eElement = (Element) nNode;
+		                    if(eElement.getElementsByTagName("id").item(0).getTextContent().equalsIgnoreCase(id)) {
+		                    	if(eElement.getElementsByTagName("pw").item(0).getTextContent().equalsIgnoreCase(pwd)) {
+		                    		oos.writeObject("로그인 성공");
+		                    		break;
+		                    	}
+		                    }else {
+		                    	oos.writeObject("로그인 실패");
+		                    	break;
+		                    }
+		                }
+		            }
+		        }
+		        catch(IOException e) {
+		            logger.info("e: {}", e);
+		        }
+				
 				oos.writeObject("테스트 완료");
 
 			}
